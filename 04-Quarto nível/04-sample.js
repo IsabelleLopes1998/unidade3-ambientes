77422ffcf9f4e05a6b88cd32ebe1f5401ba70474
Rodrigo <cabezas31@edu.unifor.br>
2024-11-06 18:56:38 -0300

    Modificação 04, Pasta 4

--- 04-Quarto nível/04-sample.js
+++ 04-Quarto nível/04-sample.js
@@ -1,6 +1,12 @@
 async function buscarPersonagem(id) {
-    const url = encodeURIComponent(`https://swapi.dev/api/people/${id}/%%%`);
+    const url = `https://swapi.dev/api/people/${id}/`;
     const resposta = await fetch(url);
+    
+    if (!resposta.ok) {
+        console.error(`Erro ao buscar personagem: ${resposta.statusText}`);
+        return;
+    }
+
     const personagem = await resposta.json();
     console.log(personagem.name);
 }
