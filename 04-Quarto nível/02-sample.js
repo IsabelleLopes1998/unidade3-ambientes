361d246b53016205804c2ad641e1a86a87c94221
Gabriel Guerreiro <ggdragon@outlook.com.br>
2024-11-08 09:02:19 -0300

    Modificação 02, Pasta 4

--- 04-Quarto nível/02-sample.js
+++ 04-Quarto nível/02-sample.js
@@ -1,1 +1,8 @@
-dddddddddddddddd
+async function buscarFilme() {
+    const resposta = await fetch("https://swapi.dev/api/films/1/");
+    const filme = await resposta.json();
+
+    console.log(filme.title.toUpperCase());
+}
+
+buscarFilme();
