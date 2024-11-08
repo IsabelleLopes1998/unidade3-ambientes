846a0f3e12e7584135848a81481408c2b18cde5b
Gabriel Guerreiro <ggdragon@outlook.com.br>
2024-11-08 09:14:16 -0300

    Modificação 06, Pasta 4

--- 04-Quarto nível/06-sample.js
+++ 04-Quarto nível/06-sample.js
@@ -1,1 +1,13 @@
-ffffffffffffff
+async function buscarNave(id) {
+    const resposta = await fetch(`https://swapi.dev/api/starships/${id}/`);
+    
+    if (!resposta.ok) {
+        console.error(`Erro: Nave com ID ${id} não encontrada. Status: ${resposta.status}`);
+        return;
+    }
+
+    const nave = await resposta.json();
+    console.log(nave.name);
+}
+
+buscarNave(10);
