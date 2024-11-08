2369fa6cf7f8617cd80b70a157ca979f0ea5afd7
Gabriel Guerreiro <ggdragon@outlook.com.br>
2024-11-08 09:16:12 -0300

    jhrgru

--- 04-Quarto nível/08-sample.js
+++ 04-Quarto nível/08-sample.js
@@ -1,15 +1,1 @@
-async function buscarPlanetaRecursivo(id) {
-    const resposta = await fetch(`https://swapi.dev/api/planets/${id}/`);
-
-    if (!resposta.ok) {
-        console.log(`Planeta com ID ${id} não encontrado ou fim da lista.`);
-        return;
-    }
-
-    const planeta = await resposta.json();
-    console.log(planeta.name);
-
-    buscarPlanetaRecursivo(id + 1);
-}
-
-buscarPlanetaRecursivo(1);
+rrrrrrrrrr
