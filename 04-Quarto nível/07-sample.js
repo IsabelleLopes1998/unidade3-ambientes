5f7cb12c43aefaf7ed467ce3856891a9b3b0596b
Gabriel Guerreiro <ggdragon@outlook.com.br>
2024-11-08 09:15:34 -0300

    Modificação 07, Pasta 4

--- 04-Quarto nível/07-sample.js
+++ 04-Quarto nível/07-sample.js
@@ -1,1 +1,10 @@
-ddddddd
+async function buscarEspecie() {
+    const resposta = await fetch("https://swapi.dev/api/species/1/");
+    const especie = await resposta.json();
+
+    const lifespan = especie.average_lifespan ? String(especie.average_lifespan) : "Desconhecido";
+
+    console.log(lifespan.toUpperCase());
+}
+
+buscarEspecie();
