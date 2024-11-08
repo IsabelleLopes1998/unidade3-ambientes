c8802abdc19ebf77926230c894c40a5df3fab70e
Gabriel Guerreiro <ggdragon@outlook.com.br>
2024-11-08 08:51:14 -0300

    Modificação 03, Pasta 4

--- 04-Quarto nível/03-sample.js
+++ 04-Quarto nível/03-sample.js
@@ -1,1 +1,7 @@
-nnbfnjdbjfjnfjbnd
+async function buscarDados() {
+    const dados = '{"name": "Luke", "idade": 23}';
+    const personagem = JSON.parse(dados);
+    console.log(personagem.name);
+}
+
+buscarDados();
