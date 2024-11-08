4ae03d6236bc80342ca68888d39ab684ce04badc
Gabriel Guerreiro <ggdragon@outlook.com.br>
2024-11-08 09:05:57 -0300

    Modificação 04, Pasta 4

--- 04-Quarto nível/04-sample.js
+++ 04-Quarto nível/04-sample.js
@@ -1,15 +1,1 @@
-async function buscarPersonagem(id) {
-    const url = `https://swapi.dev/api/people/${id}/`;
-    const resposta = await fetch(url);
-    
-    if (!resposta.ok) {
-        console.error(`Erro ao buscar personagem: ${resposta.statusText}`);
-        return;
-    }
-
-    const personagem = await resposta.json();
-    console.log(personagem.name);
-}
-
-const lukeId = 1;
-buscarPersonagem(lukeId);
+ddddddddddd
